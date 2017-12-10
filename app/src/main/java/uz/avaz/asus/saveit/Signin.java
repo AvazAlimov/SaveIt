2f93a3b1a1c10f61b793be28cac9b4bcca5ce782
mukhayy <tashpulatova97@gmail.com>
2017-12-10 14:33:18 +0500

    Mukhayyo Committed

---
+++ app/src/main/java/uz/avaz/asus/saveit/Signin.java
@@ -0,0 +1,19 @@
+package uz.avaz.asus.saveit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class Signin extends AppCompatActivity implements View.OnClickListener{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
