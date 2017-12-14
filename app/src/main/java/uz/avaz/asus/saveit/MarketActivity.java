9d36331f2eda584542c9e806e90707c60bb73f97
Asus <avazalimov95@mail.ru>
2017-12-14 15:55:23 +0500

    Avaz Committed

--- app/src/main/java/uz/avaz/asus/saveit/MarketActivity.java
+++ app/src/main/java/uz/avaz/asus/saveit/MarketActivity.java
@@ -19,6 +19,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -110,7 +111,7 @@ public class MarketActivity extends AppCompatActivity implements NavigationView.
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    loadMarkets();
+                    loadItems();
                 }
             });
         } else if (id == R.id.nav_manage) {
@@ -133,10 +134,13 @@ public class MarketActivity extends AppCompatActivity implements NavigationView.
         return false;
     }
 
-    private void loadMarkets() {
+    private void loadItems() {
         int size;
-        LinearLayout container = findViewById(R.id.container);
-        container.removeAllViews();
+        LinearLayout main_layout = findViewById(R.id.container);
+        main_layout.removeAllViews();
+        getLayoutInflater().inflate(R.layout.container_layout, main_layout);
+        LinearLayout markets_container = findViewById(R.id.markets_container);
+        LinearLayout products_container = findViewById(R.id.products_container);
         for (int i = 0; i < Tools.markets_array.size(); i++) {
             Market market = Tools.markets_array.get(i);
             size = getResources().getDimensionPixelSize(R.dimen.size_8dp);
@@ -155,7 +159,6 @@ public class MarketActivity extends AppCompatActivity implements NavigationView.
                     openMarket(v);
                 }
             });
-
             size = getResources().getDimensionPixelSize(R.dimen.size_72dp);
             ImageView image = new ImageView(this);
             image.setImageResource(R.drawable.ic_store);
@@ -166,10 +169,8 @@ public class MarketActivity extends AppCompatActivity implements NavigationView.
                 if (!market.getImage().isEmpty())
                     new Tools.DownloadImageTask(image, market.getImage()).execute(Tools.IMAGE_ADDRESS + market.getImage());
             size = getResources().getDimensionPixelSize(R.dimen.size_16dp);
-
             LinearLayout layout = new LinearLayout(this);
             layout.setOrientation(LinearLayout.VERTICAL);
-
             TextView name = new TextView(this);
             LinearLayout.LayoutParams name_lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             name_lp.setMargins(size, size, size, 0);
@@ -178,7 +179,6 @@ public class MarketActivity extends AppCompatActivity implements NavigationView.
             name.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
             name.setTextColor(Color.BLACK);
             name.setTypeface(name.getTypeface(), Typeface.BOLD);
-
             TextView address = new TextView(this);
             LinearLayout.LayoutParams address_lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             address_lp.setMargins(size, 0, size, size);
@@ -186,13 +186,80 @@ public class MarketActivity extends AppCompatActivity implements NavigationView.
             address.setText(market.getAddress());
             address.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
             address.setTextColor(Color.BLACK);
-
             layout.addView(name);
             layout.addView(address);
-
             item.addView(image);
             item.addView(layout);
-            container.addView(item);
+            markets_container.addView(item);
+        }
+        for (int i = 0; i < Tools.products_array.size(); i++) {
+            Product product = Tools.products_array.get(i);
+            @SuppressLint("SimpleDateFormat")
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+            try {
+                Date date = format.parse(product.getDate());
+                Date currentDate = new Date(System.currentTimeMillis());
+                if (currentDate.before(date)) {
+                    size = getResources().getDimensionPixelSize(R.dimen.size_8dp);
+                    LinearLayout item = new LinearLayout(this);
+                    LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    lp.gravity = Gravity.CENTER;
+                    lp.setMargins(0, size, 0, 0);
+                    item.setLayoutParams(lp);
+                    item.setTag(product.getId());
+                    item.setBackgroundResource(R.drawable.item);
+                    item.setClickable(true);
+                    item.setOrientation(LinearLayout.HORIZONTAL);
+                    size = getResources().getDimensionPixelSize(R.dimen.size_72dp);
+                    ImageView image = new ImageView(this);
+                    image.setImageResource(R.drawable.ic_groceries);
+                    image.setLayoutParams(new ViewGroup.LayoutParams(size, size));
+                    image.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                    image.setBackgroundResource(android.R.color.transparent);
+                    if (product.getImage() != null)
+                        if (!product.getImage().isEmpty())
+                            new Tools.DownloadImageTask(image, product.getImage()).execute(Tools.IMAGE_ADDRESS + product.getImage());
+                    size = getResources().getDimensionPixelSize(R.dimen.size_16dp);
+
+                    LinearLayout layout = new LinearLayout(this);
+                    layout.setOrientation(LinearLayout.VERTICAL);
+
+                    TextView name = new TextView(this);
+                    LinearLayout.LayoutParams name_lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    name_lp.setMargins(size, size, size, 0);
+                    name.setLayoutParams(name_lp);
+                    name.setText(product.getName());
+                    name.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+                    name.setTextColor(Color.BLACK);
+                    name.setTypeface(name.getTypeface(), Typeface.BOLD);
+
+                    TextView address = new TextView(this);
+                    LinearLayout.LayoutParams address_lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    address_lp.setMargins(size, 0, size, size);
+                    address.setLayoutParams(address_lp);
+                    address.setText(
+                            String.format("%s: %s\n%s: %s\n%s: %s%s",
+                                    getString(R.string.category),
+                                    Tools.findCategoryName(product.getCategory()),
+                                    getString(R.string.expirydate),
+                                    product.getDate(),
+                                    getString(R.string.price),
+                                    product.getNew_price(),
+                                    getString(R.string.sum))
+                    );
+                    address.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+                    address.setTextColor(Color.BLACK);
+
+                    layout.addView(name);
+                    layout.addView(address);
+
+                    item.addView(image);
+                    item.addView(layout);
+                    products_container.addView(item);
+                }
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -211,7 +278,6 @@ public class MarketActivity extends AppCompatActivity implements NavigationView.
             try {
                 Date date = format.parse(product.getDate());
                 Date currentDate = new Date(System.currentTimeMillis());
-                Log.e("DATE", date.toString() + " * " + currentDate.toString());
                 if (currentDate.before(date)) {
                     size = getResources().getDimensionPixelSize(R.dimen.size_8dp);
                     LinearLayout item = new LinearLayout(this);
@@ -366,5 +432,17 @@ public class MarketActivity extends AppCompatActivity implements NavigationView.
     public void goToAddProductWindow(View view) {
         Intent intent = new Intent(this, AddProductActivity.class);
         startActivity(intent);
+    }
+
+    public void switchContainers(View view) {
+        if (findViewById(R.id.markets_container).getVisibility() == View.VISIBLE) {
+            findViewById(R.id.markets_container).setVisibility(View.GONE);
+            findViewById(R.id.products_container).setVisibility(View.VISIBLE);
+            ((Button) findViewById(R.id.change_button)).setText(getString(R.string.markets));
+        } else {
+            findViewById(R.id.markets_container).setVisibility(View.VISIBLE);
+            findViewById(R.id.products_container).setVisibility(View.GONE);
+            ((Button) findViewById(R.id.change_button)).setText(getString(R.string.products));
+        }
     }
 }
